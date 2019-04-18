ab86e0c046f09820b66ddb3d8f0987c808bef4fa
evandrofr <38050761+evandrofr@users.noreply.github.com>
2019-04-18 17:05:59 -0300

    le_scan modificado

--- meu_projeto/Projeto1.py
+++ meu_projeto/Projeto1.py
@@ -30,7 +30,7 @@ atraso = 1.5E9
 area = 0.0
 check_delay = False 
 bumper = None
-dist = None
+dist = []
 
 
 def roda_todo_frame(imagem):
@@ -81,7 +81,10 @@ def scaneou_bumper(data):
 
 def scaneou_scan(dado):
     global dist
-    dist = (np.array(dado.ranges))[0]
+    dist = (np.array(dado.ranges))
+    for n in range(len(dist)):
+        if dist[n] < dado.range_min:
+            dist[n] = 1
     return dist
 
     
@@ -108,71 +111,79 @@ if __name__=="__main__":
 
 
 
-    velocidade_angular_p = Twist(Vector3(0,0,0), Vector3(0,0,pi/10))
-    velocidade_angular_n = Twist(Vector3(0,0,0), Vector3(0,0,-pi/10))
+    velocidade_angular_p = Twist(Vector3(0,0,0), Vector3(0,0,-pi/10))
+    velocidade_angular_n = Twist(Vector3(0,0,0), Vector3(0,0,pi/10))
     velocidade_re = Twist(Vector3(-0.2,0,0), Vector3(0,0,0))
-    velocidade_fuga = Twist(Vector3(-0.3,0,0), Vector3(0,0,-0.2))
+    velocidade_fuga = Twist(Vector3(-0.3,0,0), Vector3(0,0,0.5))
     velocidade = Twist(Vector3(0.2,0,0), Vector3(0,0,0))
     parado = Twist(Vector3(0,0,0),Vector3(0,0,0))
 
     while not rospy.is_shutdown():
 
-        vel = Twist(Vector3(0.1,0,0), Vector3(0,0,0))
-        print(media)
+        vel = Twist(Vector3(0.15,0,0), Vector3(0,0,0))
+        # print(media)
 
         if len(media) != 0 and len(centro_cor) != 0:
             # print("Média dos vermelhos: {0}, {1}".format(media[0], media[1]))
             # print("Centro dos vermelhos: {0}, {1}".format(centro_cor[0], centro_cor[1]))
 
 
-            if media[0] - centro_cor[0] < -30:
-                vel = Twist(Vector3(0.1,0,0), Vector3(0,0,-0.5))
-                print("Esquerda")
+            if media[0] - centro_cor[0] < -40:
+                vel = Twist(Vector3(0.15,0,0), Vector3(0,0,0.5))
+                print("Vermelho a Esquerda")
 
-            elif media[0] - centro_cor[0] > 30:
-                vel = Twist(Vector3(0.1,0,0), Vector3(0,0,0.5))
-                print("Direita")
+            elif media[0] - centro_cor[0] > 40:
+                vel = Twist(Vector3(0.15,0,0), Vector3(0,0,-0.5))
+                print("Vermelho a Direita")
 
-            elif (media[0] - centro_cor[0] > -30) or (media[0] - centro_cor[0] < 30):
-                vel = Twist(Vector3(0.1,0,0), Vector3(0,0,0))
-                print("Reto")
+            elif (media[0] - centro_cor[0] > -40) or (media[0] - centro_cor[0] < 40):
+                vel = Twist(Vector3(0.15,0,0), Vector3(0,0,0))
+                print("Vermelho Reto")
 
 
         if viu_bird:
+            print("Bird! Fuga!")
+            velocidade_saida.publish(parado)
+            rospy.sleep(1)
             velocidade_saida.publish(velocidade_fuga)
             rospy.sleep(1)
             viu_bird = False
 
-
-        if dist != None:
-            if (min(dist[0:10]) < 0.3 or min(dist[349:359]) < 0.3):
-                velocidade_saida.publish(parado)
-                rospy.sleep(1)
+        # print("dist",dist)
+        if dist != []:
+            if ((min(dist[0:20]) < 0.25) or (min(dist[339:359]) < 0.25)):
+                print("Quase batendo: Frente")
+                # velocidade_saida.publish(parado)
+                # rospy.sleep(1)
                 velocidade_saida.publish(velocidade_fuga)
-                rospy.sleep(0.5)
+                rospy.sleep(1)
                 velocidade_saida.publish(velocidade_angular_n)
                 rospy.sleep(1)
 
-            if min(dist[80:10]) < 0.3:
-                desvio = Twist(Vector3(0.5,0,0), Vector3(0,0,-0.5))
+            if (min(dist[79:99]) < 0.25):
+                print("Quase batendo: Esquerda")
+                desvio = Twist(Vector3(0,0,0), Vector3(0,0,-0.7))
                 velocidade_saida.publish(desvio)
-                rospy.sleep(2)
+                rospy.sleep(1)
 
 
-            if min(dist[170:190]) < 0.3:
+            if (min(dist[169:189]) < 0.25):
+                print("Quase batendo: Costas")
                 velocidade_saida.publish(velocidade)
-                rospy.sleep(2)
+                rospy.sleep(1)
 
 
-            if min(dist[260:280]) < 0.3:
-                desvio = Twist(Vector3(0.5,0,0), Vector3(0,0,0.5))
+            if (min(dist[259:279]) < 0.25):
+                print("Quase batendo: Direita")
+                desvio = Twist(Vector3(0,0,0), Vector3(0,0,0.7))
                 velocidade_saida.publish(desvio)
-                rospy.sleep(2)
+                rospy.sleep(1)
 
 
 
 
         if bumper == 1:
+            print("Bumper 1")
             parado = Twist(Vector3(0,0,0),Vector3(0,0,0))
             velocidade_saida.publish(parado)
             rospy.sleep(2)
@@ -180,12 +191,13 @@ if __name__=="__main__":
             velocidade_saida.publish(velocidade_re)
             rospy.sleep(2)
 
-            velocidade_angular = Twist(Vector3(0,0,0), Vector3(0,0,-pi/10))
+            velocidade_angular = Twist(Vector3(0,0,0), Vector3(0,0,pi/10))
             velocidade_saida.publish(velocidade_angular)
             rospy.sleep(3)
             bumper = 0
             
         elif bumper == 2:
+            print("Bumper 2")
             parado = Twist(Vector3(0,0,0),Vector3(0,0,0))
             velocidade_saida.publish(parado)
             rospy.sleep(2)
@@ -193,12 +205,26 @@ if __name__=="__main__":
             velocidade_saida.publish(velocidade_re)
             rospy.sleep(2)
 
-            velocidade_angular = Twist(Vector3(0,0,0), Vector3(0,0,pi/10))
+            velocidade_angular = Twist(Vector3(0,0,0), Vector3(0,0,-pi/10))
             velocidade_saida.publish(velocidade_angular)
             rospy.sleep(3)
             bumper = 0
 
         elif bumper == 3:
+            print("Bumper 3")
+            parado = Twist(Vector3(0,0,0),Vector3(0,0,0))
+            velocidade_saida.publish(parado)
+            rospy.sleep(2)
+            velocidade_saida.publish(velocidade)
+            rospy.sleep(2)
+
+            velocidade_angular = Twist(Vector3(0,0,0), Vector3(0,0,pi/10))
+            velocidade_saida.publish(velocidade_angular)
+            rospy.sleep(3)
+            bumper = 0  
+
+        elif bumper == 4:
+            print("Bumper 4")
             parado = Twist(Vector3(0,0,0),Vector3(0,0,0))
             velocidade_saida.publish(parado)
             rospy.sleep(2)
@@ -206,18 +232,6 @@ if __name__=="__main__":
             rospy.sleep(2)
 
             velocidade_angular = Twist(Vector3(0,0,0), Vector3(0,0,-pi/10))
-            velocidade_saida.publish(velocidade_angular)
-            rospy.sleep(3)
-            bumper = 0  
-
-        elif bumper == 4:
-            parado = Twist(Vector3(0,0,0),Vector3(0,0,0))
-            velocidade_saida.publish(parado)
-            rospy.sleep(2)
-            velocidade_saida.publish(velocidade)
-            rospy.sleep(2)
-
-            velocidade_angular = Twist(Vector3(0,0,0), Vector3(0,0,pi/10))
             velocidade_saida.publish(velocidade_angular)
             rospy.sleep(3)
             bumper = 0
